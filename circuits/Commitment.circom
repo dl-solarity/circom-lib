8eb22949a234b73faa09aa249f45eaea611a4139
RuslanProgrammer <ruslankasheparov@gmail.com>
2024-03-15 08:45:08 +0100

    Added Commitment circuit

---
+++ circuits/Commitment.circom
@@ -0,0 +1,56 @@
+pragma circom 2.0.0;
+
+include "../node_modules/circomlib/circuits/poseidon.circom";
+
+/*
+ * Hash1 = Poseidon(nullifier)
+ */
+template Hash1() {
+    signal input a;
+
+    signal output out;
+
+    component h = Poseidon(1);
+    h.inputs[0] <== a;
+
+    out <== h.out;
+}
+
+/*
+ * Hash2 = Poseidon(nullifier | secret)
+ */
+template Hash2() {
+    signal input a;
+    signal input b;
+
+    signal output out;
+
+    component h = Poseidon(2);
+    h.inputs[0] <== a;
+    h.inputs[1] <== b;
+
+    out <== h.out;
+}
+
+/*
+ * This circuit is a simple commitment circuit that takes a nullifier and a secret and returns a commitment.
+ * The commitment is the result of hashing the secret and the nullifier together using a Poseidon hash function.
+*/
+template Commitment() {
+    signal input nullifier;
+    signal input secret;
+
+    signal output commitment;
+    signal output nullifierHash;
+
+    component commitmentHash = Hash2();
+    commitmentHash.a <== nullifier;
+    commitmentHash.b <== secret;
+
+    commitment <== commitmentHash.out;
+
+    component nullifierHasher = Hash1();
+    nullifierHasher.a <== nullifier;
+
+    nullifierHash <== nullifierHasher.out;
+}
---
+++ test/Commitment.test.ts
@@ -0,0 +1,47 @@
+import { CircomJS } from "@zefi/circomjs";
+import { expect } from "chai";
+import { ethers } from "hardhat";
+
+import { Reverter } from "./helpers/reverter";
+import { generateCalldata } from "./helpers/zk-helper";
+
+import { CommitmentMockVerifier } from "@ethers-v6";
+
+describe("Commitment", () => {
+  const reverter = new Reverter();
+  const circom = new CircomJS();
+
+  const commitmentCircuit = circom.getCircuit("CommitmentMock");
+
+  let commitmentVerifier: CommitmentMockVerifier;
+
+  before("setup", async () => {
+    const CommitmentMockVerifier = await ethers.getContractFactory("CommitmentMockVerifier");
+
+    commitmentVerifier = await CommitmentMockVerifier.deploy();
+
+    await reverter.snapshot();
+  });
+
+  afterEach(reverter.revert);
+
+  it("should return commitment and hash of nullifier", async () => {
+    const proofStruct = await commitmentCircuit.genProof({
+      nullifier: 1,
+      secret: 2,
+    });
+
+    // Poseidon(1, 2)
+    expect(proofStruct.publicSignals[0]).to.equal(
+      "7853200120776062878684798364095072458815029376092732009249414926327459813530",
+    );
+    // Poseidon(1)
+    expect(proofStruct.publicSignals[1]).to.equal(
+      "18586133768512220936620570745912940619677854269274689475585506675881198879027",
+    );
+
+    const [pA, pB, pC, publicSignals] = await generateCalldata(proofStruct);
+
+    expect(await commitmentVerifier.verifyProof(pA, pB, pC, publicSignals as any)).to.be.true;
+  });
+});
