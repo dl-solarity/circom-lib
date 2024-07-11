a414cb1ca92575adc82719259ed560471b547721
Oleg Komendant <44612825+Hrom131@users.noreply.github.com>
2024-07-11 19:03:19 +0300

    Update hardhat-zkit and fix typization (#10)

--- test/blinders/Commitment.test.ts
+++ test/blinders/Commitment.test.ts
@@ -1,17 +1,16 @@
 import { expect } from "chai";
 import { ethers, zkit } from "hardhat";
 
-import { CircuitZKit } from "@solarity/zkit";
-
 import { Reverter } from "../helpers/reverter";
 
 import { CommitmentVerifierVerifier } from "@ethers-v6";
+import { CommitmentVerifier } from "@zkit";
 
 describe("Commitment", () => {
   const reverter = new Reverter();
 
   let commitmentVerifier: CommitmentVerifierVerifier;
-  let commitmentCircuit: CircuitZKit;
+  let commitmentCircuit: CommitmentVerifier;
 
   before("setup", async () => {
     const CommitmentMockVerifier = await ethers.getContractFactory("CommitmentVerifierVerifier");
@@ -31,16 +30,16 @@ describe("Commitment", () => {
     });
 
     // Poseidon(1, 2)
-    expect(proofStruct.publicSignals[0]).to.equal(
+    expect(proofStruct.publicSignals.commitment).to.equal(
       "7853200120776062878684798364095072458815029376092732009249414926327459813530",
     );
     // Poseidon(1)
-    expect(proofStruct.publicSignals[1]).to.equal(
+    expect(proofStruct.publicSignals.nullifierHash).to.equal(
       "18586133768512220936620570745912940619677854269274689475585506675881198879027",
     );
 
     const [pA, pB, pC, publicSignals] = await commitmentCircuit.generateCalldata(proofStruct);
 
-    expect(await commitmentVerifier.verifyProof(pA, pB, pC, publicSignals as any)).to.be.true;
+    expect(await commitmentVerifier.verifyProof(pA, pB, pC, publicSignals)).to.be.true;
   });
 });
