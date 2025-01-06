3e416e405aedcdb62c072aa76323a7e44d2d7c33
aritkulova <aritkulova.yuliia@gmail.com>
2025-01-06 16:13:35 +0200

    updated tests

--- test/blinders/Commitment.test.ts
+++ test/blinders/Commitment.test.ts
@@ -3,42 +3,44 @@ import { ethers, zkit } from "hardhat";
 
 import { Reverter } from "../helpers/reverter";
 
-import { CommitmentVerifierGroth16Verifier } from "@ethers-v6";
-import { CommitmentVerifier } from "@zkit";
+import { CommitmentGroth16Verifier } from "@ethers-v6";
+import { Commitment } from "@zkit";
 
 describe("Commitment", () => {
   const reverter = new Reverter();
 
-  let commitmentVerifier: CommitmentVerifierGroth16Verifier;
-  let commitmentCircuit: CommitmentVerifier;
+  let verifier: CommitmentGroth16Verifier;
+  let circuit: Commitment;
 
   before("setup", async () => {
-    const CommitmentMockVerifier = await ethers.getContractFactory("CommitmentVerifierGroth16Verifier");
+    const CommitmentMock = await ethers.getContractFactory("CommitmentGroth16Verifier");
 
-    commitmentVerifier = await CommitmentMockVerifier.deploy();
-    commitmentCircuit = await zkit.getCircuit("CommitmentVerifier");
+    verifier = await CommitmentMock.deploy();
+    circuit = await zkit.getCircuit("Commitment");
 
     await reverter.snapshot();
   });
 
   afterEach(reverter.revert);
 
-  it("should return commitment and hash of nullifier", async () => {
-    await expect(commitmentCircuit)
+  it("should return verifier and hash of nullifier", async () => {
+    await expect(circuit)
       .with.witnessInputs({
         nullifier: 1,
         secret: 2,
+        dummy: 0,
       })
       .to.have.witnessOutputs({
         commitment: "7853200120776062878684798364095072458815029376092732009249414926327459813530",
         nullifierHash: "18586133768512220936620570745912940619677854269274689475585506675881198879027",
       });
 
-    const proofStruct = await commitmentCircuit.generateProof({
+    const proofStruct = await circuit.generateProof({
       nullifier: 1,
       secret: 2,
+      dummy: 0,
     });
 
-    await expect(commitmentCircuit).to.useSolidityVerifier(commitmentVerifier).and.verifyProof(proofStruct);
+    await expect(circuit).to.useSolidityVerifier(verifier).and.verifyProof(proofStruct);
   });
 });
